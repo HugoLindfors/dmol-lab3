ff382faaa3db44dbe4ec873c288df38bf9063530
Hugo Lindfors <HugoLindfors@users.noreply.github.com>
2025-10-01 17:26:38 +0200

    minor changes

--- mygcd.m
+++ mygcd.m
@@ -1,25 +1,23 @@
-% mygcd.m
+% mygcd_recursive.m
 
 % Hugo Lindfors (huglih251)
 
 % mygcd() accepts a dividend and a divisor as parameters and returns the greatest common divisor
 function quotient = mygcd(dividend, divisor)
 
-remainder = 1; % remainder has to be set to any non 0 value
+remainder = 1; % This disables an annoying error message – remainder has to be set to any non 0 value
 
-while remainder ~= 0
+if divisor ~= 0
+    quotient = floor(dividend/divisor);
+    remainder = mod(dividend, divisor);
+else
+    clc
+    disp("ERROR! Dividing by 0 is undefined!")
+end
 
-    if divisor ~= 0
-        quotient = floor(dividend/divisor);
-        remainder = mod(dividend, divisor);
-    else
-        clc
-        disp("ERROR! Dividing by 0 is undefined!")
-    end
-
-    if remainder ~= 0 && divisor ~= 0
-        dividend = divisor;
-        divisor = quotient;
-    end
+if remainder ~= 0 && divisor ~= 0
+    dividend = divisor;
+    divisor = quotient;
+    mygcd_recursive(dividend, divisor)
 end
 end
